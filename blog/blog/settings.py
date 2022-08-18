41a15c9b56f4c4391b6cbaeafd8bae3013160f41
Yuanchu Zhang <yuanchuzhang@YuanchudeMacBook-Pro.local>
2022-08-18 22:10:23 +0800

    create user model

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -192,3 +192,7 @@ LOGGING = {
         },
     }
 }
+
+# 替换系统的User 来使用我们自己的User
+# 配置信息为 '子应用名.模型类名'
+AUTH_USER_MODEL = 'users.User'
--- blog/users/models.py
+++ blog/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    # 手机号
+    mobile = models.CharField(max_length=11, unique=True, blank=False)
+    # 头像信息
+    avatar = models.ImageField(upload_to='avatar/%Y%m%d/', blank=True)
+    # 简介信息
+    user_desc = models.CharField(max_length=1000, blank=True)
+
+    class Meta:
+        db_table = 'tb_users'  # 修改表名
+        verbose_name = '用户管理'  # admin后台显示
+        verbose_name_plural = verbose_name  # admin后台显示
+
+    def __str__(self):
+        return self.mobile
+
